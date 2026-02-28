31d36d180b3e64e6509cd86e51fe4a09fd6d20b5
xhannahh <hannahhe1211@gmail.com>
2026-02-28 17:39:48 -0600

    added doxygen comments to webserver

--- WebServer.cpp
+++ WebServer.cpp
@@ -1,19 +1,38 @@
+/**
+ * @file WebServer.cpp
+ * @brief Implementation of the WebServer class.
+ */
+
 #include "WebServer.h"
 
+/**
+ * @brief Constructs a WebServer with default idle state.
+ * @param serverName The name/identifier for this server.
+ */
 WebServer::WebServer(std::string serverName)
 {
     name = serverName;
     reqStartTime = 0;
     timeRemaining = 0;
+    busy = false;
 }
 
+/**
+ * @brief Assigns a new request to this server and marks it busy.
+ * @param r The request to be processed.
+ * @param currTime The current system clock cycle.
+ */
 void WebServer::assignRequest(Request r, int currTime)
 {
     currentRequest = r;
     reqStartTime = currTime;
     timeRemaining = r.time;
+    busy = true;
 }
 
+/**
+ * @brief Decrements the remaining processing time by one cycle.
+ */
 void WebServer::processCycle()
 {
     if (timeRemaining > 0)
@@ -22,21 +41,37 @@ void WebServer::processCycle()
     }
 }
 
+/**
+ * @brief Resets the server to an idle state.
+ */
 void WebServer::clearRequest()
 {
     timeRemaining = 0;
+    busy = false;
 }
 
+/**
+ * @brief Returns whether the server is currently busy.
+ * @return True if the server is processing a request.
+ */
 bool WebServer::isBusy() const
 {
-    return timeRemaining > 0;
+    return busy;
 }
 
+/**
+ * @brief Returns whether the server has finished its current request.
+ * @return True if the server was busy and has no time remaining.
+ */
 bool WebServer::isDone() const
 {
-    return timeRemaining == 0;
+    return busy && timeRemaining == 0;
 }
 
+/**
+ * @brief Returns the server's name.
+ * @return The name string of this server.
+ */
 std::string WebServer::getName() const
 {
     return name;
--- WebServer.h
+++ WebServer.h
@@ -1,23 +1,66 @@
+/**
+ * @file WebServer.h
+ * @brief Defines the WebServer class that processes requests.
+ */
+
 #ifndef WEBSERVER_H
 #define WEBSERVER_H
 #include "Request.h"
 #include <string>
 
+/**
+ * @class WebServer
+ * @brief Simulates a web server that processes one request at a time.
+ */
 class WebServer
 {
 private:
-    std::string name;
-    Request currentRequest;
-    int reqStartTime;
-    int timeRemaining;
+    std::string name;       ///< Name/identifier of the server.
+    Request currentRequest; ///< The request currently being processed.
+    int reqStartTime;       ///< The clock cycle when the current request was assigned.
+    int timeRemaining;      ///< Clock cycles remaining to finish the current request.
+    bool busy;              ///< Whether the server is currently processing a request.
 
 public:
+    /**
+     * @brief Constructs a WebServer with the given name.
+     * @param serverName The name/identifier for this server.
+     */
     WebServer(std::string serverName);
+
+    /**
+     * @brief Assigns a request to this server.
+     * @param r The request to process.
+     * @param currTime The current system clock cycle.
+     */
     void assignRequest(Request r, int currTime);
+
+    /**
+     * @brief Advances processing by one clock cycle.
+     */
     void processCycle();
+
+    /**
+     * @brief Clears the current request and marks the server as idle.
+     */
     void clearRequest();
+
+    /**
+     * @brief Checks if the server is currently processing a request.
+     * @return True if the server is busy, false otherwise.
+     */
     bool isBusy() const;
+
+    /**
+     * @brief Checks if the server has finished its current request.
+     * @return True if the server was busy and has completed processing.
+     */
     bool isDone() const;
+
+    /**
+     * @brief Gets the name of this server.
+     * @return The server name string.
+     */
     std::string getName() const;
 };
 
